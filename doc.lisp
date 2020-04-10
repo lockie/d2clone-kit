909592478fbdfbcc4c2cbc15fee3a868bcd92d69
Andrew Kravchuk <awkravchuk@gmail.com>
2020-04-10 10:01:15 +0300

    added CI configuration to automatically build docs

--- doc.lisp
+++ doc.lisp
@@ -1,7 +1,6 @@
 #!/usr/bin/env -S sbcl --script
 
 (load (merge-pathnames ".sbclrc" (user-homedir-pathname)))
-(ql:quickload :staple)
-(ql:quickload :staple-markdown)
+(ql:quickload '(:staple :staple-markdown))
 
 (staple:generate :d2clone-kit :if-exists :supersede :images '(#P"d2clone.png"))
