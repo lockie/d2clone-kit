047b6d37fa78d40a9c4d63330fbf716929b1c89e
Andrew Kravchuk <awkravchuk@gmail.com>
2020-04-20 19:07:42 +0300

    removed unnecessary file

--- build.lisp
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env -S sbcl --script
-
-(load (merge-pathnames ".sbclrc" (user-homedir-pathname)))
-(ql:quickload :deploy)
-
-(push (truename ".") asdf:*central-registry*)
-(asdf:make :d2clone-kit)
