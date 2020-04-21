6f3ab3c9cd870a033bd86ee410b21fd30d5ab216
Andrew Kravchuk <awkravchuk@gmail.com>
2020-04-21 07:07:03 +0300

    minor mana system improvement

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Removed ubiquitous mana system component.
+
 ### Fixed
 - Fixed compilation on CCL.
 
--- src/mana-system.lisp
+++ src/mana-system.lisp
@@ -6,8 +6,8 @@
   (:documentation "Handles mana points."))
 
 (defcomponent mana mana
-  (maximum-mana 1d0 :type double-float)
-  (current-mana 1d0 :type double-float))
+  (maximum-mana nil :type double-float)
+  (current-mana nil :type double-float))
 
 (defmethod make-component ((system mana-system) entity &rest parameters)
   (destructuring-bind (&key current maximum) parameters
