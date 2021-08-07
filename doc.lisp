0e5397597cc4e9a09cf61508b35a9568c7694846
Andrew Kravchuk <awkravchuk@gmail.com>
2021-08-07 18:53:59 +0300

    more improvements in docs CI pipeline

--- doc.lisp
+++ doc.lisp
@@ -5,4 +5,9 @@
 (ql:quickload '(:staple :staple-markdown :d2clone-kit))
 (pushnew :docs *features*)
 
-(staple:generate :d2clone-kit :if-exists :supersede :images '(#P"d2clone.png"))
+(staple:generate :d2clone-kit
+                 :if-exists :supersede :images '(#P"d2clone.png")
+                 :output-directory
+                 (if (member "-p" (uiop:command-line-arguments) :test #'string=)
+                     #P"public/"
+                     #P"docs/"))
