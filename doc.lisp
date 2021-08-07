a142a1295716216a99b8ee71a960aa038983d11b
Andrew Kravchuk <awkravchuk@gmail.com>
2021-08-07 18:35:02 +0300

    improvements in docs CI pipeline

--- doc.lisp
+++ doc.lisp
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S sbcl --script
 
 (load (merge-pathnames ".sbclrc" (user-homedir-pathname)))
-(ql:quickload '(:staple :staple-markdown))
+(push (truename ".") asdf:*central-registry*)
+(ql:quickload '(:staple :staple-markdown :d2clone-kit))
 (pushnew :docs *features*)
 
 (staple:generate :d2clone-kit :if-exists :supersede :images '(#P"d2clone.png"))
